16282bcee95af601c356c328482accbae537fa5f
Nvawo Alamar <nyawoalamar5@gmail.com>
2026-09-09 11:23:52 +0300

    Add State City Amenity Place and Review models

--- models/amenity.py
+++ models/amenity.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
-"""Module docstring."""
+"""Defines the Amenity class."""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Amenity model."""
+
+    name = ""
--- models/review.py
+++ models/review.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
-"""Module docstring."""
+"""Defines the Review class."""
+
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """Review model."""
+
+    place_id = ""
+    user_id = ""
+    text = ""
